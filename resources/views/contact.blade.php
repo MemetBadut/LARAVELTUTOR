e25eecf00d452e0137ec5f98998773dac1015eff
MemetBadut <pasekm06@gmail.com>
2025-09-17 10:44:56 +0800

    Alamak sedap ni part2

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Service Centre</title>
+</head>
+<body>
+    <p>Hubungi :<strong> nasihytam@gmail.com</strong> </p>
+    <a href="/">Back Home</a>
+</body>
+</html>
