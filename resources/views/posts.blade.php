8c7d60d5c1211372e781cea3092dc0d1419af967
MemetBadut <pasekm06@gmail.com>
2025-09-23 14:14:58 +0800

    benerin posts blade biar ga error bagian link untuk tampilan post blog

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,7 +4,7 @@
 
     @foreach ($posts as $post)
         <article class="py-8 max-w-md border-b border-amber-950">
-            <a href="{{ route('posts.detail', ['id' => $post['id']]) }}" class="hover:underline">
+            <a href="{{ route('posts.detail', ['slug' => $post['slug']]) }}" class="hover:underline">
                 <h2 class="mb-3 text-3xl font-bold text-gray"> {{ $post['judul'] }} </h2>
             </a>
             <div class="text-base text-gray-300">
@@ -13,7 +13,7 @@
             <p class="text-justify my-3.5 font-light">
                 {{ Str::limit($post['body'], 150) }}
             </p>
-            <a href="{{ route('posts.detail', ['id' => $post['id']]) }}" class="font-medium text-blue-500">Read More &raquo;</a>
+            <a href="{{ route('posts.detail', ['slug' => $post['slug']]) }}" class="font-medium text-blue-500">Read More &raquo;</a>
         </article>
     @endforeach
 
--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::get('/posts', function () {
         'posts' => [
             [
                 'id' => '1',
+                'slug' => 'judul-artikel-1',
                 'judul' => 'Artikel 1',
                 'author' => 'Bapak Mulprobo',
                 'body' => '  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus incidunt libero obcaecati iusto veniam
@@ -39,6 +40,7 @@ Route::get('/posts', function () {
             ],
             [
                 'id' => '2',
+                'slug' => 'judul-artikel-2',
                 'judul' => 'Artikel 2',
                 'author' => 'Bapak Yonobowo',
                 'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore delectus incidunt rem repudiandae ducimus!
@@ -50,11 +52,12 @@ Route::get('/posts', function () {
 })->name('posts');
 
 
-Route::get('/posts/{id}', function ($id) {
-   $post = [
+Route::get('/posts/{slug}', function ($slug) {
+    $post = [
         [
             'id' => '1',
             'judul' => 'Artikel 1',
+            'slug' => 'judul-artikel-1',
             'author' => 'Bapak Mulprobo',
             'body' => '  Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusamus incidunt libero obcaecati iusto veniam
                 accusantium tempora officia deserunt animi veritatis repudiandae atque fugiat dolore, aspernatur nam debitis
@@ -63,6 +66,7 @@ Route::get('/posts/{id}', function ($id) {
         [
             'id' => '2',
             'judul' => 'Artikel 2',
+            'slug' => 'judul-artikel-1',
             'author' => 'Bapak Yonobowo',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore delectus incidunt rem repudiandae ducimus!
                 Placeat nemo iusto, libero magnam necessitatibus sequi dolorum atque hic. Nemo accusamus sapiente non
@@ -70,8 +74,8 @@ Route::get('/posts/{id}', function ($id) {
         ],
     ];
 
-    $post = Arr::first($post, function($post) use ($id){
-        return $post['id'] == $id;
+    $post = Arr::first($post, function ($post) use ($slug) {
+        return $post['id'] == $slug;
     });
 
     return view('post', ['title' => 'post page', 'dashboard' => 'Single Post', 'post' => $post]);
